0cbfb59b4a18f60b5b78623db23e054896bfb98e
Oluyemisi-Awolade <155662724+Oluyemisi-Awolade@users.noreply.github.com>
2026-06-30 10:40:04 +0100

    Delete MainActivity.kt

--- MainActivity.kt
+++
@@ -1,6 +0,0 @@
-// android/app/src/main/kotlin/com/ludopromax/app/MainActivity.kt
-package com.ludopromax.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()

57cf5211b95681e0c0b1665bcbf7a813306c36d7
Oluyemisi-Awolade <155662724+Oluyemisi-Awolade@users.noreply.github.com>
2026-06-30 10:38:01 +0100

    Add MainActivity class for Flutter integration

--- android/app/src/main/kotlin/com/ludopromax/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/ludopromax/app/MainActivity.kt
@@ -1,1 +1,6 @@
+// android/app/src/main/kotlin/com/ludopromax/app/MainActivity.kt
+package com.ludopromax.app
 
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
